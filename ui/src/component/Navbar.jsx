368279f1866b1252046011ed741dd7842eea4466
sahil <sahilgomes2003@gmail.com>
2024-02-08 23:59:16 +0530

    added after: hover tooltip using tailwind

--- ui/src/component/Navbar.jsx
+++ ui/src/component/Navbar.jsx
@@ -8,7 +8,7 @@ import logout_logo from "../assets/logoutLogo.svg";
 export default function Navbar() {
   const navigate = useNavigate();
 
-  function logoutHandle(){
+  function logoutHandle() {
     localStorage.clear();
     navigate("/login");
   }
@@ -17,11 +17,11 @@ export default function Navbar() {
     <>
       <div className="navbar-container w-16 border-r-[0.5px] relative">
         <nav>
-          <Link to="/" className=" mt-4 h-[55px]"><img src={main_logo} alt="ProtonBaaS" className="w-[48px] h-[48px]"/></Link>
-          <NavLink to={"/dbdata"} className="hover:bg-base-300"><img src={dbdata_logo} alt="dbdata_logo" className="w-[48px] h-[48px]"/></NavLink>
-          <NavLink to={"/logs"} className="hover:bg-base-300"><img src={editor_logo} alt="editor_logo" className="w-[48px] h-[48px]"/></NavLink>
-          <NavLink to={"/settings"} className="hover:bg-base-300"><img src={settings_logo} alt="settings_logo" /></NavLink>
-          <button className="absolute bottom-0 p-4 hover:bg-base-300" onClick={logoutHandle}><img src={logout_logo} alt="logout"/></button>
+          <Link to="/" className=" mt-4 h-[55px]"><img src={main_logo} alt="ProtonBaaS" className="w-[48px] h-[48px]" /></Link>
+          <NavLink to={"/dbdata"} className="hover:bg-base-300 relative after:content-['dbdata'] after:absolute after:hidden hover:after:inline after:top-[25%] after:left-[120%]"><img src={dbdata_logo} alt="dbdata_logo" className="w-[48px] h-[48px]" /></NavLink>
+          <NavLink to={"/logs"} className="hover:bg-base-300 after:content-['logs'] relative after:absolute after:hidden hover:after:inline after:top-[25%] after:left-[120%]"><img src={editor_logo} alt="editor_logo" className="w-[48px] h-[48px]" /></NavLink>
+          <NavLink to={"/settings"} className="hover:bg-base-300 relative after:content-['settings'] after:absolute after:hidden hover:after:inline after:top-[25%] after:left-[120%]"><img src={settings_logo} alt="settings_logo" /></NavLink>
+          <button className="absolute bottom-0 p-4 hover:bg-base-300" onClick={logoutHandle}><img src={logout_logo} alt="logout" /></button>
         </nav>
       </div>
     </>
