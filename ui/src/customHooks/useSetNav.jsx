4deab6d9ba168e05ff856355c3c0276efcbab4c1
sahil <sahilgomes2003@gmail.com>
2024-02-10 16:47:33 +0530

    hook sets nav state

--- ui/src/customHooks/useSetNav.jsx
+++ ui/src/customHooks/useSetNav.jsx
@@ -2,17 +2,15 @@ import { useEffect } from "react";
 import { useOutletContext } from "react-router-dom";
 
 export default function useSetNav() {
-    const { setShowNav } = useOutletContext();
+    const { setShowNav,setPopmessage } = useOutletContext();
 
-    useEffect(()=>{
+    useEffect(() => {
         if (location.hash === "#/login") {
-            setTimeout(()=>setShowNav(false),5000);
+            setShowNav(false);
         }
         else {
-            setTimeout(()=>setShowNav(true),5000);
+            setShowNav(true);
         }
-        console.log("i am useSetNav hook");
-        console.log(location.hash)
-    },[]);
-    return;
+    }, []);
+    return setPopmessage;
 }
