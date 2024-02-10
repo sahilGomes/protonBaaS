bc281be40c517e46be02e540ca7ff66d2dc7654b
sahil <sahilgomes2003@gmail.com>
2024-02-10 17:07:01 +0530

    searchbar component added

---
+++ ui/src/component/Searchbar.jsx
@@ -0,0 +1,11 @@
+export default function Searchbar({ value, placeholder, changeSearchvalue,inputWidth }) {
+  function handleinput(e) {
+    changeSearchvalue(e.target.value);
+  }
+
+  return (
+    <div className="h-[75px] flex justify-center items-center border-b-[1px]">
+      <input type="text" placeholder={placeholder} value={value} onChange={handleinput} className={`input input-bordered w-[${inputWidth}] h-[35px] hover:w-[80%]`} />
+    </div>
+  )
+}
