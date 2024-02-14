dd2204c473b9899e928ac6651e0efc67d871e631
sahil <sahilgomes2003@gmail.com>
2024-02-14 22:29:47 +0530

    1)added a data maping fuction individually. 2)add props now using Link as manually hadling current selected component

--- ui/src/component/Editorpage/Collection.jsx
+++ ui/src/component/Editorpage/Collection.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import Searchbar from "../Searchbar";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export default function Collection({ dataArray }) {
+export default function Collection({ dataArray, activeCollection, setActiveCollection }) {
   const [searchvalue, setSearchvalue] = useState("");
   if (searchvalue !== "") {
     const searchTerm = searchvalue.toLowerCase();
@@ -10,12 +10,26 @@ export default function Collection({ dataArray }) {
     dataArray = dataArray.filter(name => name.toLowerCase().includes(searchTerm));
   }
 
+  function handleClickOnCollection(event){
+    const url = new URL(event.target.href);
+    const params = new URLSearchParams(url.hash.slice(13));
+    setActiveCollection(params.get("collectionId"));
+  }
+
+  function mapData(value, index) {
+    return (
+      <div className={`collectionholder ${value === activeCollection ? "active" : ""} w-[80%] mx-auto text-center hover:bg-base-300 rounded-md my-2 py-1`} key={index}>
+        <Link to={`/collections?collectionId=${value}`} className="inline-block w-[100%] select-none" onClick={handleClickOnCollection}>{value}</Link>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col w-[100%] relative h-[100%]">
       <Searchbar value={searchvalue} placeholder="🔍 Search collections" changeSearchvalue={setSearchvalue} inputWidth="80%" />
       <div className="flex flex-col overflow-y-auto max-h-[81%]">
-        {dataArray.map((value, index) => <div className="collectionholder w-[80%] mx-auto text-center hover:bg-base-300 rounded-md my-2 py-1" key={index}><NavLink to={value}>{value}</NavLink></div>)}
-        {dataArray.length?null:<div className="text-center">No such collections</div>}
+        {dataArray.map(mapData)}
+        {dataArray.length ? null : <div className="text-center w-[80%] mx-auto my-2">No collections available.</div>}
       </div>
       <button className="btn btn-active sticky bottom-0  w-[80%] mx-auto">➕ Add Collection</button>
     </div>
