3bbae77c122650494a24869d2800b2387eb6f4ce
sahil <sahilgomes2003@gmail.com>
2024-02-24 23:24:31 +0530

    searchbar input removed unused hover

--- ui/src/component/Searchbar.jsx
+++ ui/src/component/Searchbar.jsx
@@ -5,7 +5,7 @@ export default function Searchbar({ value, placeholder, changeSearchvalue,inputW
 
   return (
     <div className="h-[75px] flex justify-center items-center border-b-[1px]">
-      <input type="text" placeholder={placeholder} value={value} onChange={handleinput} className={`input input-bordered w-[${inputWidth}] h-[35px] hover:w-[80%]`} />
+      <input type="text" placeholder={placeholder} value={value} onChange={handleinput} className={`input input-bordered w-[${inputWidth}] h-[35px]`} />
     </div>
   )
 }
