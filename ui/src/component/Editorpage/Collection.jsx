370c0ddaca3ef818063b0d62d69b5d91b0ecd072
sahil <sahilgomes2003@gmail.com>
2024-02-24 19:47:10 +0530

    added some comments

--- ui/src/component/Editorpage/Collection.jsx
+++ ui/src/component/Editorpage/Collection.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Searchbar from "../Searchbar";
 import { Link } from "react-router-dom";
 
+// left side compenent
 export default function Collection({ dataArray, activeCollection, setActiveCollection }) {
   const [searchvalue, setSearchvalue] = useState("");
   if (searchvalue !== "") {
@@ -10,12 +11,14 @@ export default function Collection({ dataArray, activeCollection, setActiveColle
     dataArray = dataArray.filter(name => name.toLowerCase().includes(searchTerm));
   }
 
+  // function to hadle collection name link clicks i.e is to open side panel
   function handleClickOnCollection(event){
     const url = new URL(event.target.href);
     const params = new URLSearchParams(url.hash.slice(13));
     setActiveCollection(params.get("collectionId"));
   }
 
+  // function to mapdata as link for collection name list
   function mapData(value, index) {
     return (
       <div className={`collectionholder ${value === activeCollection ? "active" : ""} w-[80%] mx-auto text-center hover:bg-base-300 rounded-md my-2 py-1`} key={index}>
