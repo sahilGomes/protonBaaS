9f3546b3d402fc407c86d9e3e0693a1dc7dd30bd
sahil <sahilgomes2003@gmail.com>
2024-03-09 22:07:58 +0530

    backend setup done

---
+++ core/app.js
@@ -0,0 +1,32 @@
+import createError from "http-errors";
+import express from "express";
+import { dirname,join } from "node:path";
+import cookieParser from "cookie-parser";
+import { fileURLToPath } from "node:url";
+import morgan from "morgan";
+
+// setting middlewares to be used in application
+let app = express();
+
+app.use(morgan('dev')); //logging
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use("/pub",express.static(join(dirname(fileURLToPath(import.meta.url)),'public')));
+
+// routing according to path
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+
+
+// handling error
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//   res.status(err.status || 500);
+//   res.send("error")
+// });
+
+export {app};
