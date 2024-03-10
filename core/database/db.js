0391c6171b6e0a3263058eb3294f24faacee19e7
sahil <sahilgomes2003@gmail.com>
2024-03-10 17:27:23 +0530

    added mongoose and done its setup

---
+++ core/database/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectionDB = async ()=>{
+    await mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`,{
+        minPoolSize:10
+    }).then((mongoose)=>{
+        console.log(`Monogobd connected:${mongoose.connection.host}`);
+    }).catch(e=>{
+        console.log("Monogdb connection failed as:\n",e);
+        process.exit(1);
+    });
+}
+
+export default connectionDB;
