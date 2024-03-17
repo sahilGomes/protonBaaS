273b709673ef62512ed7f95bce0bafa5ea1dd719
sahil <sahilgomes2003@gmail.com>
2024-03-18 01:16:17 +0530

    added post get to records logic

--- core/routes/collection.routes.js
+++ core/routes/collection.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { listCollection, createCollection,updateCollection, deleteCollection } from "../controllers/collection.controllers.js";
+import { createRecord, listRecord } from "../controllers/record.controllers.js";
 
 // route for /api/collections/
 const router = express.Router();
 
 router.route("/").get(listCollection).post(createCollection);
 router.route("/:collectionId").get(listCollection).put(updateCollection).delete(deleteCollection);
+router.route("/:collectionId/records/").get(listRecord).post(createRecord);
+// router.route("/:collectionId/records/:recordId").get();
 
 export default router;
