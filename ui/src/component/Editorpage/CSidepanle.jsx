6e5ca29ec0da4a2fc329e799901a7cb1762025dc
sahil <sahilgomes2003@gmail.com>
2024-03-19 16:52:33 +0530

    done with collection making and updating

--- ui/src/component/Editorpage/CSidepanle.jsx
+++ ui/src/component/Editorpage/CSidepanle.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 
 
@@ -7,9 +7,19 @@ export default function CSidepanle({ showPanelData, setShowPanelData }) {
     const { setPopmessage } = useOutletContext();
     const [collectionSchema, setCollectionSchema] = useState(showPanelData.singleCollectionSchema);
     const [collectionName, setCollectionName] = useState({ "name": showPanelData.activeCollection });
-    const [schemaString, setSchemaString] = useState();
-    const [ruleString, setRuleString] = useState();
+    const [schemaString, setSchemaString] = useState(JSON.stringify(showPanelData.singleCollectionSchema.schema));
+    const [ruleString, setRuleString] = useState(JSON.stringify(showPanelData.singleCollectionSchema.rules));
     const [loading, setLoading] = useState(false);
+
+    useEffect(()=>{
+        if (showPanelData.action === "edit") {
+            let arraySchema = JSON.parse(schemaString);
+            setSchemaString(JSON.stringify(arraySchema.map(obj=>{
+                delete obj._id;
+                return obj;
+            })));
+        }
+    },[]);
 
     function handleCancelButton(e) {
         setShowPanelData(null);
@@ -37,16 +47,17 @@ export default function CSidepanle({ showPanelData, setShowPanelData }) {
             // Parse the JSON string to obtain the object
             let extractedSchemaObject = JSON.parse(schemaString);
             let extractedRuleObject = JSON.parse(ruleString);
-            let datatoSend = { ...collectionName, ...collectionSchema, schema: extractedSchemaObject, rules: extractedRuleObject }
-            console.log(JSON.stringify(datatoSend))
+            let datatoSend = { ...collectionName, schema: extractedSchemaObject, rules: extractedRuleObject }
             const abortController = new AbortController();
             const fetchData = async () => {
                 const signal = abortController.signal;
 
                 try {
-                    const response = await fetch(`${location.protocol}//${location.hostname}:8080/api/collections`,
+                    let url_point = showPanelData.action === "new" ? "/api/collections" : `/api/collections/${showPanelData.singleCollectionSchema._id}`;
+                    let fetchMethod = showPanelData.action === "new" ? "POST" : "PUT";
+                    const response = await fetch(`${location.protocol}//${location.hostname}:8080${url_point}`,
                         {
-                            method: 'POSt',
+                            method: fetchMethod,
                             headers: {
                                 "Content-Type": "application/json",
                                 "Authorization": JSON.parse(localStorage.getItem("admin_auth")).token
@@ -59,7 +70,7 @@ export default function CSidepanle({ showPanelData, setShowPanelData }) {
                         if (response.status === 200) {
                             setLoading(false);
                             setShowPanelData(null);
-                            setPopmessage("Created new Collection.REFRESH")
+                            setPopmessage(showPanelData.action === "new"?"Created new Collection.REFRESH":"Updated.REFRESH")
                             let overlayout = document.getElementsByClassName("overlayout-collection")[0];
                             overlayout.classList.remove("show");
                         }
@@ -104,17 +115,17 @@ export default function CSidepanle({ showPanelData, setShowPanelData }) {
                     </div>
                     <div>
                         <div>Fields</div>
-                        <textarea name="collection-schema" className=" textarea textarea-bordered w-[100%]" id="" cols="50" rows="5" placeholder="put schema e.g:-[{'name':'posts','type':'string'},{'name':'messages','type':'string'}]" onChange={handleonChangeCollectionSchema} required value={showPanelData.action !== "new" ? showPanelData.schema : undefined}></textarea>
+                        <textarea name="collection-schema" className=" textarea textarea-bordered w-[100%]" id="" cols="50" rows="5" placeholder="put schema e.g:-[{'name':'posts','type':'string'},{'name':'messages','type':'string'}]" onChange={handleonChangeCollectionSchema} required value={showPanelData.action !== "new" ? schemaString : undefined}></textarea>
 
                     </div>
                     <div>
                         <div>Api rules</div>
-                        <textarea name="collection-schema" className="textarea textarea-bordered w-[100%]" id="" cols="50" rows="5" placeholder='put rules e.g:-{"list rule":"","view rule":"","update rule":"","delete rule":"","create rule":""}' onChange={handleonChangeRules} value={showPanelData.action !== "new" ? showPanelData.rule : undefined}></textarea>
+                        <textarea name="collection-schema" className="textarea textarea-bordered w-[100%]" id="" cols="50" rows="5" placeholder='put rules e.g:-{"list rule":"","view rule":"","update rule":"","delete rule":"","create rule":""}' onChange={handleonChangeRules} value={showPanelData.action !== "new" ? ruleString : undefined}></textarea>
                     </div>
                 </div>
                 <div className="base h-[10%] flex justify-end gap-4 items-center">
                     <button className="btn btn-neutral" onClick={handleCancelButton}>cancel</button>
-                    <button className="btn btn-primary" onClick={handleSave} disabled={loading?true:false}>{loading ? "Saving" : "Save"}</button>
+                    <button className="btn btn-primary" onClick={handleSave} disabled={loading ? true : false}>{loading ? "Saving" : "Save"}</button>
                 </div>
             </div>
         </>
--- ui/src/component/Editorpage/Documentstable.jsx
+++ ui/src/component/Editorpage/Documentstable.jsx
@@ -1,5 +1,99 @@
-export default function Documentstable() {
+import Searchbar from "../Searchbar";
+import settingsLogo from "../../assets/collectionSettigsLogo.svg";
+import refreshLogo from "../../assets/refreshLogo.svg";
+import { useEffect, useState } from "react";
+import TSidepanel from "./TSidepanel";
+import TableWithData from "./TableWithData";
+
+export default function Documentstable({ activeCollection, collectionData, setParentShowPanelData }) {
+  const [searchvalue, setSearchvalue] = useState("");
+  const [recordDataarray, setRecordDataArray] = useState([]);
+  const [showPanelData, setShowPanelData] = useState(null);
+
+  let schema = collectionData.filter((objSchema) => {
+    return activeCollection === objSchema.name
+  })[0];
+
+  useEffect(() => {
+    // make fetch to records and update recordDataArray
+    console.log("fetch to records of: ", activeCollection)
+  }, [activeCollection]);
+
+  function handleNewDataButton(e) {
+    setShowPanelData({ action: 'new', schema: schema, recordId: null, recordData: null });
+    let button = document.getElementsByClassName("overlayout-table-data")[0];
+    button.style.setProperty("right", "0px", "important");
+    button.classList.remove('left-[100%]');
+    button.classList.add("show");
+  }
+
+  function handleSettingButton(e) {
+    setParentShowPanelData({ action: "edit", singleCollectionSchema: schema ,activeCollection:activeCollection});
+    let overlayout = document.getElementsByClassName("overlayout-collection")[0];
+    overlayout.classList.add("show");
+  }
+
+  // refresh the data on refresh click
+  function refreshDataHandler(e) {
+    // call fetch and update recordData
+    console.log("refresh")
+  }
+
+  // to get table columns name
+  const tableColumnArray = []
+  collectionData.forEach(element => {
+    tableColumnArray.push(element["name"]);
+  });
+
   return (
-    <div>Documents</div>
+    <>
+      <div className="table_navbar flex justify-between h-[40px] items-center">
+        <div className="nav_left_container flex gap-2 text-xl">
+          <div>Collection /</div>
+          <div>{activeCollection}</div>
+          <button className="settings hover:bg-accent-content rounded-full relative after:content-['settings']  after:z-10 after:absolute after:hidden hover:after:inline hover:after after:top-[100%] after:left-[0] text-sm" onClick={handleSettingButton}><img src={settingsLogo} alt="logo" className="h-[35px] w-6 px-[2px] mx-2" /></button>
+          <button className="refresh hover:bg-accent-content rounded-full relative after:content-['refresh']  after:z-10 after:absolute after:hidden hover:after:inline hover:after after:top-[100%] after:left-[0] text-sm" onClick={refreshDataHandler}><img src={refreshLogo} alt="logo" className="h-[35px] w-6 px-[2px] mx-2" /></button>
+        </div>
+        <div className="bg-primary p-1 rounded-lg text-primary-content cursor-pointer select-none" onClick={handleNewDataButton}>➕ New data</div>
+      </div>
+      <Searchbar value={searchvalue} placeholder="Search terms . . . ." changeSearchvalue={setSearchvalue} inputWidth="100%" />
+      <div className="table-container overflow-x-auto pt-1">
+        <table className="table table-lg bg-accent-neutral">
+
+          <thead>
+            <tr>
+              {tableColumnArray.map((element, index) => <th key={index}>{element}</th>)}
+            </tr>
+          </thead>
+          <tbody>
+
+            <tr className="hover">
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+            </tr>
+
+            <tr className="hover">
+              <th>2</th>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support Technician</td>
+              <td>Purple</td>
+            </tr>
+
+            <tr className="hover">
+              <th>3</th>
+              <td>Brice Swyre</td>
+              <td>Tax Accountant</td>
+              <td>Red</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      {/* <TableWithData recordDataArray={recordDataarray} schema={schema}/> */}
+      <div className="overlayout-table-data absolute w-0 h-[100%] top-0 left-[100%] transition-all duration-100 bg-base-300 overflow-y-auto p-2 bord border-l border-accent flex flex-col">
+        {showPanelData !== null && <TSidepanel showPanelData={showPanelData} setShowPanelData={setShowPanelData} />}
+      </div>
+    </>
   )
 }
